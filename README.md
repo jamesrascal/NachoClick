3ccc9aa757cefc3c57c03dca3d93be4b130a0955
jamesrascal <roger@gentry.ch>
2022-06-27 08:24:28 -0700

    Refactor to single route
    Refactored the flask routes to use a single route in place of the multiple functions. As we are redirecting the URL from the loaded database we can do this from a single route.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Nacho.Click short Nacho URLs
 
+Takes input from a user - Long URL - makes a short version of the link using Python. 
+
+## Install
+1. Clone Repo
+2. Install requirements.txt using pip
+3. Run using gunicorn -b localhost:8000 -w 4 ncahoclick:app
+
--- main.py
+++ main.py
@@ -1,58 +1,45 @@
+import os
+import json
 import string
 import secrets
 from flask import Flask, render_template, redirect, request
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
-import json
-import os
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+baseURL = 'http://nacho.click/'
+nachDB = 'urldb.json'
 
-
-class MyForm(FlaskForm):
+class ShortURLForm(FlaskForm):
     userURL = StringField('Enter URL to be shortened', validators=[DataRequired()])
 
-def makeSecret():
-    alphabet = string.ascii_letters + string.digits
-    thisSecret = ''.join(secrets.choice(alphabet) for i in range(8))
-    return thisSecret
-def loadnachodata():
-    # Opening JSON file
-    f = open('urldb.json')
+# The main Nach.Click functions
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>',methods=['GET','POST'])
+def entry_point(path):
+    # Attempts to load the DB fails if it's not on disk.
+    try:
+        f = open(nachDB)
+    except:
+        return('Loading of the NachoDB failed')
     nachodata = json.load(f)
-    return nachodata
-@app.route('/')
-def entry_point():
-    print(request.remote_addr)
-    form = MyForm()
-    return render_template('main.html', thisSecret=makeSecret(), form=form)
-
-# Displays after creating url
-@app.route('/surl', methods=['POST'])
-def makeShort():
-    print(request.remote_addr)
-    nachodata = loadnachodata()
-    shorty = makeSecret() # Uses secrets to make the secret
-    nachodata[shorty]=request.form['userURL'] # Save to dict then redirect.
-    with open('urldb.json', 'w') as f:
-        json.dump(nachodata, f)
-    return render_template('url.html', shortURL=f"http://nacho.click/l/{shorty}")
-
-# Redirection route
-@app.route('/l/<goURL>')
-def gotoURL(goURL):
-    nachodata = loadnachodata()
-    print(goURL)
-    if goURL in nachodata:
-        return redirect(nachodata[goURL], code=302)
+    if path in nachodata:
+        return redirect(nachodata[path], code=302)
+    if request.method == 'GET':
+        return render_template('main.html', form=ShortURLForm())
+    if request.method == 'POST':
+        shorty = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(8))  # Uses secrets to make the secret
+        try:
+            nachodata[shorty] = request.form['userURL']  # Save to dict then redirect.
+        except:
+            return render_template('error.html', message='Something went wrong saving URL (ERROR URLFS01)')
+        with open('urldb.json', 'w') as f:
+            json.dump(nachodata, f)
+        return render_template('url.html', shortURL=f"{baseURL}{shorty}")
     else:
-        return redirect('/', code=302)
-
-@app.route('/account')
-def account():
-    print('This is the account page')
-    return render_template('login.html')
+        return render_template('error.html', message='Something went wrong?')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
